14d21738b9f9ab35253a81b24e0e596a724d2fb8
Aniruddha Kembhavi <anik@allenai.org>
2022-02-09 22:49:13 -0800

    Rendering bug fix

--- source.js
+++ source.js
@@ -26,29 +26,29 @@ function beginActivity(){
 
  $("#card1").css("background-color",color1);
  $("#card2").css("background-color",color2);
- $("#card2").css("top","200px");
+ $("#card2").css("top","600px");
  $("#cardArea").on("click",function(){
   if(cardState!=1){
    cardState=1;
    //togglePosition();
-   $("#card1").animate({top: "-=200"}, 150, function() {cardState=0;togglePosition();});
-   $("#card2").animate({top: "-=200"}, 150, function() {togglePosition2();});
+   $("#card1").animate({top: "-=600"}, 150, function() {cardState=0;togglePosition();});
+   $("#card2").animate({top: "-=600"}, 150, function() {togglePosition2();});
   }//if
  });//click function
 
  $("#buttonArea").empty();
  $("#buttonArea").append('<div id="nextButton">NEXT</div>');
  $("#nextButton").on("click",function(){
-  beginActivity();  
+  beginActivity();
  });//click function
 }//beginactivity
 
 function togglePosition(){
- if($("#card1").position().top==-200){$("#card1").css("top","200px");};
+ if($("#card1").position().top==-600){$("#card1").css("top","600px");};
 }//toggle
 
 function togglePosition2(){
- if($("#card2").position().top==-200){$("#card2").css("top","200px");};
+ if($("#card2").position().top==-600){$("#card2").css("top","600px");};
 }//toggle2
 
 });
